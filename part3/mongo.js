f46059d36c312d995bf0c5c977340b7c2dc5c9de
Giancarlo Massoni <massonicarlo55@gmail.com>
2026-01-15 10:32:23 -0800

    uibuild

---
+++ part3/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://massonicarlo55:${password}@cluster0.vsr5wur.mongodb.net/noteApp?appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "html is easy",
+  important: true,
+});
+
+Note.find({}).then((res) => {
+  res.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
+
+// note.save().then((res) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
